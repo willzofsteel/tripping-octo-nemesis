5f506b81c33d1df94555ced57bfec737b53c8ad7
william <william.andrews@gmail.com>
2013-10-16 22:33:06 -0700

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Funnycat::Application.config.secret_key_base = '05e1c9a54771a43aa9827515232e987ecaee1cecaabb3a5935bbfe91ac27c76161798e48309a71027b936634933ba92a1a8f89c2e33dfe1423a1f468b8af8a4b'
